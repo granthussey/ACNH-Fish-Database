47f33a57617da415a090b6e25699aed7a5ee7aa7
granthussey <16183823+granthussey@users.noreply.github.com>
2020-04-01 20:15:03 -0400

    Initial commit MVP

---
+++ webapp/dashtable_app.py
@@ -0,0 +1,334 @@
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+from dash.dependencies import Input, Output
+import dash_table
+
+import pandas as pd
+import numpy as np
+
+# Set stylesheet and initialize app
+
+external_stylesheets = ["https://codepen.io/chriddyp/pen/bWLwgP.css"]
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+# print(external_stylesheets)
+
+# app = dash.Dash(__name__)
+
+# Read in fish and bugs csv
+# df = pd.read_csv('https://plotly.github.io/datasets/country_indicators.csv')
+
+fish = pd.read_pickle("/Users/granthussey/github/ACNL/data/acnl_fish.pkl")
+# bugs = pd.read_pickle('/Users/granthussey/github/ACNL/data/acnl_bug.pkl')
+
+available_fish = fish[fish.columns[0]].unique()
+# available_bugs =
+available_months = fish.loc[:, "All":"December"].columns.unique()
+
+
+# testing_df = pd.DataFrame({"gay": [1, 2, 5, 2], "straight": [2134, 213, 12, 2]})
+
+
+def df_cols_to_dashtable_cols(df):
+    return [{"name": i, "id": i} for i in df.columns]
+
+
+def df_cols_to_dropdown_options(iterable):
+    return [{"label": i, "value": i} for i in iterable]
+
+
+def generate_table(dataframe, max_rows=100):
+    return html.Table(
+        children=[
+            html.Thead(html.Tr([html.Th(col) for col in dataframe.columns])),
+            html.Tbody(
+                [
+                    html.Tr(
+                        [html.Td(dataframe.iloc[i][col]) for col in dataframe.columns]
+                    )
+                    for i in range(min(len(dataframe), max_rows))
+                ]
+            ),
+        ]
+    )
+
+
+def filter_backend_table(backend_table):
+
+    if backend_table.columns[0] == "Bug":
+        df = backend_table.filter(
+            items=[
+                backend_table.columns[0],
+                "Location",
+                "Price",
+                "Active start hours",
+                "Active end hour",
+            ]
+        )
+
+    elif backend_table.columns[0] == "Fish":
+        df = backend_table.filter(
+            items=[
+                backend_table.columns[0],
+                "Location",
+                "Shadow size",
+                "Price",
+                "Active start hours",
+                "Active end hour",
+            ]
+        )
+
+    return df
+
+
+def filter_given_months(backend_table, months):
+
+    """
+    This needs to take either a list or a str and deal with it properly.
+    """
+
+    # remove columns from backend_table that we wont want to show
+    presentable_df = filter_backend_table(backend_table)
+
+    # print('This is the length of presentable_df ' + str(len(presentable_df)))
+
+    try:
+        # If user inputs a single month
+        if isinstance(months, str):
+            selected = backend_table[months]
+            return presentable_df.loc[selected]
+
+        # If user inputs more than one month
+        elif isinstance(months, list):
+
+            # initialize an all-true vector to use later
+            selected = pd.Series([True] * len(presentable_df))
+            for each_month in months:
+                selected = selected & backend_table[each_month]
+
+            return presentable_df.loc[selected]
+
+        # If empty string (user deleted everything from Dropdown)
+        elif not months:
+            pass
+
+    except Exception:
+        print("There was an exception in filter_given_months() function")
+        pass
+
+
+def filter_given_fish(backend_table, selected_fish):
+
+    # remove columns from backend_table that we wont want to show
+    presentable_df = filter_backend_table(backend_table)
+
+    # print('This is the length of presentable_df ' + str(len(presentable_df)))
+
+    try:
+        # If user inputs a single month
+        if isinstance(selected_fish, str):
+            selected = backend_table["Fish"] == selected_fish
+            return presentable_df.loc[selected]
+
+        # If user inputs more than one month
+        elif isinstance(selected_fish, list):
+
+            # initialize an all-true vector to use later
+            selected = pd.Series([True] * len(presentable_df))
+
+            for each_fish in selected_fish:
+                selected = selected & backend_table["Fish"] == selected_fish
+
+            return presentable_df.loc[selected]
+
+        # If empty string (user deleted everything from Dropdown)
+        elif not selected_fish:
+            pass
+
+    except Exception:
+        print("There was an exception in filter_given_fish() function")
+        pass
+
+
+testing_df = filter_backend_table(fish)
+
+app.layout = html.Div(
+    [
+        # Initialize the filters top two columns
+        html.Div(
+            children=[
+                html.H3(children="The Fish Database"),
+                html.Div(
+                    children=[
+                        """
+                Welcome to the premier fish database.
+                Choose from the drop downs below to filter the table below.
+                """
+                    ],
+                    style={"padding": "10px 5px"},
+                ),
+                html.Div(
+                    children=[
+                        dcc.Dropdown(
+                            id="fish-dropdown",
+                            options=df_cols_to_dropdown_options(available_fish),
+                            placeholder="Filter by individual fish",
+                            multi=True,
+                        )
+                    ],
+                    style={"width": "49%", "display": "inline-block"},
+                ),
+                html.Div(
+                    children=[
+                        dcc.Dropdown(
+                            id="month-dropdown",
+                            options=df_cols_to_dropdown_options(available_months),
+                            # value="",
+                            placeholder="Filter by month fish is active",
+                            multi=True,
+                        ),
+                    ],
+                    style={"width": "49%", "float": "right", "display": "inline-block"},
+                ),
+            ],
+            style={
+                "borderBottom": "thin lightgrey solid",
+                "backgroundColor": "rgb(250, 250, 250)",
+                "padding": "10px 5px",
+            },
+        ),
+        # 1. style_data_conditional
+        # 2. style_data
+        # 3. style_filter_conditional
+        # 4. style_filter
+        # 5. style_header_conditional
+        # 6. style_header
+        # 7. style_cell_conditional
+        # 8. style_cell
+        html.Div(
+            children=dash_table.DataTable(
+                id="doot",
+                columns=df_cols_to_dashtable_cols(testing_df),
+                data=testing_df.to_dict("records"),
+                # Remove the certical lines
+                style_as_list_view=True,
+                # Make the columns
+                style_header={"textAlign": "left", "fontWeight": "bold"},
+                style_data_conditional=[
+                    {
+                        "if": {"row_index": "odd"},
+                        "backgroundColor": "rgb(248, 248, 248)",
+                    }
+                ],
+                style_data={"font": "Arial"}
+                # style_data={"border": "1px solid black"}
+                # style_cell_conditional=[
+                #     {"if": {"column_id": c}, "textAlign": "left"}
+                #     for c in [
+                #         "Fish",
+                #         "Location",
+                #         "Shadow size",
+                #         "Active start hours",
+                #         "Active end hour",
+                #     ]
+                # ],
+            ),
+            style={
+                "marginBottom": 50,
+                "marginTop": 25,
+                "marginRight": 25,
+                "marginLeft": 25,
+            },
+            # style={"margin": "auto"},
+        ),
+        # Initialize the table for display
+        # html.Div(id='my-table', children=generate_table(fish), style={'width':'80%'
+        #         }
+        #     )
+    ]
+)
+
+
+def return_original_fish_table():
+    presentable_table = filter_backend_table(fish)
+    return (
+        presentable_table.to_dict("records"),
+        df_cols_to_dashtable_cols(presentable_table),
+    )
+
+
+@app.callback(
+    [Output("doot", "data"), Output("doot", "columns")],
+    [Input("month-dropdown", "value")],
+)
+def update_table(months_to_filter):
+
+    # logic for the beginning where you have NoneType for "value"
+    if months_to_filter is None:
+        print()
+        print(months_to_filter)
+        print(type(months_to_filter))
+        print()
+        return return_original_fish_table()
+
+    # now, make sure that it is a list
+    elif isinstance(months_to_filter, str) or isinstance(months_to_filter, list):
+
+        if len(months_to_filter) == 0:
+            print()
+            print(months_to_filter)
+            print(type(months_to_filter))
+            print()
+            return return_original_fish_table()
+
+        else:
+            print()
+            print(months_to_filter)
+            print(type(months_to_filter))
+            print()
+            filtered_df = filter_given_months(fish, months_to_filter)
+            return (
+                filtered_df.to_dict("records"),
+                df_cols_to_dashtable_cols(filtered_df),
+            )
+
+
+# @app.callback(
+#     [Output("doot", "data"), Output("doot", "columns")],
+#     [Input("fish-dropdown", "value")],
+# )
+# def update_table_by_fish(fish_to_filter):
+
+#     # logic for the beginning where you have NoneType for "value"
+#     if fish_to_filter is None:
+#         print()
+#         print(fish_to_filter)
+#         print(type(fish_to_filter))
+#         print()
+#         return return_original_fish_table()
+
+#     # now, make sure that it is a list
+#     elif isinstance(fish_to_filter, str) or isinstance(fish_to_filter, list):
+
+#         if len(fish_to_filter) == 0:
+#             print()
+#             print(fish_to_filter)
+#             print(type(fish_to_filter))
+#             print()
+#             return return_original_fish_table()
+
+#         else:
+#             print()
+#             print(fish_to_filter)
+#             print(type(fish_to_filter))
+#             print()
+#             filtered_df = filter_given_months(fish, fish_to_filter)
+#             return (
+#                 filtered_df.to_dict("records"),
+#                 df_cols_to_dashtable_cols(filtered_df),
+#             )
+
+
+if __name__ == "__main__":
+    app.run_server(debug=True)
+    # app.run_server(debug=True, dev_tools_hot_reload=False)
