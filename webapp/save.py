f708a020ea20a9f617989b2de15e8cc66188d47c
granthussey <16183823+granthussey@users.noreply.github.com>
2020-04-04 16:19:49 -0400

    Create save.py

---
+++ webapp/save.py
@@ -0,0 +1,21 @@
+app.index_string = """
+<!DOCTYPE html>
+<html>
+    <head>
+        {%metas%}
+        <title>{%title%}</title>
+        {%favicon%}
+        {%css%}
+    </head>
+    <body>
+        <div>My Custom header</div>
+        {%app_entry%}
+        <footer>
+            {%config%}
+            {%scripts%}
+            {%renderer%}
+        </footer>
+        <div>My Custom footer</div>
+    </body>
+</html>
+"""
